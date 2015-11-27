8fb3619d27269d69c06fa8da96d187283df2c04d
Mark Hobson <markhobson@gmail.com>
2015-11-27 17:47:37 +0000

    Moved AppComponent to separate file

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,6 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-
-var AppComponent = React.createClass({
-	render: function() {
-		return (
-			<h1>Yak Shaving</h1>
-		);
-	}
-});
+var AppComponent = require("./AppComponent");
 
 ReactDOM.render(
 	<AppComponent/>,
