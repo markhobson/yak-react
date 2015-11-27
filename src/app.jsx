8ca08ecfd62cf0c457825732603c93d317bf768c
Mark Hobson <markhobson@gmail.com>
2015-11-27 17:38:08 +0000

    Introduced AppComponent

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,15 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+var AppComponent = React.createClass({
+	render: function() {
+		return (
+			<h1>Yak Shaving</h1>
+		);
+	}
+});
+
 ReactDOM.render(
-	<h1>Yak Shaving</h1>,
+	<AppComponent/>,
 	document.getElementById("app")
 );
