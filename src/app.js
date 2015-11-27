f8b623014138f92e3a859014f11209bf1ef8ca2b
Mark Hobson <markhobson@gmail.com>
2015-11-27 16:33:38 +0000

    Use JSX syntax with help from babelify

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 ReactDOM.render(
-	React.createElement("h1", {}, "Yak Shaving"),
+	<h1>Yak Shaving</h1>,
 	document.getElementById("app")
 );
