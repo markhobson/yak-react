0c669dac45ed9f022b6164a082156e1d985d23cd
Mark Hobson <markhobson@gmail.com>
2015-11-27 18:02:52 +0000

    Pulled up yaks into variable

--- src/YakList.jsx
+++ src/YakList.jsx
@@ -2,13 +2,21 @@ var React = require("react");
 
 var YakList = React.createClass({
 	render: function() {
+		var yaks = [
+			{id: 1, name: "A"},
+			{id: 2, name: "B"},
+			{id: 3, name: "C"}
+		];
+		var yakNodes = yaks.map(function(yak) {
+			return (
+				<li key={yak.id}>{yak.name}</li>
+			);
+		});
 		return (
 			<div>
 				<h2>Yaks</h2>
 				<ul>
-					<li>A</li>
-					<li>B</li>
-					<li>C</li>
+					{yakNodes}
 				</ul>
 			</div>
 		);
